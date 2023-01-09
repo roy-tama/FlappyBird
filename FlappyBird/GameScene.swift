18007076a892f9c2ab5bcb88f1f90694f750cca7
tatsu.maki <tatsuya.spring0414@gmail.com>
2023-01-09 10:04:29 +0900

    lesson7 chapter7.1 done

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,215 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by 牧野達也 on 2023/01/06.
+//
+
+//import UIKit
+import SpriteKit
+
+class GameScene: SKScene {
+
+    var scrollNode: SKNode!
+    var wallNode: SKNode!
+    var bird: SKSpriteNode!
+    
+    // SKView上にシーンが表示された時に呼ばれるメソッド
+    override func didMove(to view: SKView) {
+
+        // 重力を設定
+        physicsWorld.gravity = CGVector(dx: 0, dy: -4)
+        // 背景色の設定
+        backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+        
+        // スクロールするスプライトの親ノード
+        scrollNode = SKNode()
+        addChild(scrollNode)
+        
+        wallNode = SKNode()
+        scrollNode.addChild(wallNode)
+        
+        // 各種スプライトの生成
+        setupGround()
+        setupCloud()
+        setupWall()
+        setupBird()
+        
+    }
+    
+    func setupGround() {
+        // 地面の画像を読み込む
+        let groundTexture = SKTexture(imageNamed: "ground")
+        groundTexture.filteringMode = .nearest
+        
+        // 必要な枚数を計算
+        let needNumber = Int(self.frame.size.width / groundTexture.size().width) + 2
+        // スクロールするアクションを作成
+        // 左方向に画像一枚分スクロールさせるアクション
+        let moveGround = SKAction.moveBy(x: -groundTexture.size().width, y: 0, duration: 5)
+
+        // 元の位置に戻すアクション
+        let resetGround = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0)
+        
+        // 左にスクロール->元の位置->左にスクロールと無限に繰り返すアクション
+        let repeatScrollGround = SKAction.repeatForever(SKAction.sequence([moveGround,resetGround]))
+        
+        // groundのスプライトを配置する
+        for i in 0..<needNumber {
+
+            // テクスチャを指定してスプライトを作成する
+            let sprite = SKSpriteNode(texture: groundTexture)
+//          let groundSprite = SKSpriteNode(texture: groundTexture)
+
+            // スプライトの表示する位置を指定する
+            sprite.position = CGPoint(
+                x: groundTexture.size().width / 2 + groundTexture.size().width * CGFloat(i),
+                y: groundTexture.size().height / 2
+            )
+
+            // スプライトにアクションを設定する
+            sprite.run(repeatScrollGround)
+
+            // シーンにスプライトを追加する
+            addChild(sprite)
+        }
+    }
+    
+    func setupCloud() {
+        // 雲の画像を読み込む
+        let cloudTexture = SKTexture(imageNamed: "cloud")
+        cloudTexture.filteringMode = .nearest
+        
+        // 必要な枚数を計算
+        let needNumber = Int(self.frame.size.width / cloudTexture.size().width) + 2
+        
+        // スクロールするアクションを作成
+        // 左方向に画像一枚分スクロールさせるアクション
+        let movecloud = SKAction.moveBy(x: -cloudTexture.size().width, y: 0, duration: 20)
+
+        // 元の位置に戻すアクション
+        let resetcloud = SKAction.moveBy(x: cloudTexture.size().width, y: 0, duration: 0)
+
+        // 左にスクロール->元の位置->左にスクロールと無限に繰り返すアクション
+        let repeatScrollCloud = SKAction.repeatForever(SKAction.sequence([movecloud,resetcloud]))
+
+        // cloudのスプライトを配置する
+        for i in 0..<needNumber {
+
+            // テクスチャを指定してスプライトを作成する
+            let sprite = SKSpriteNode(texture: cloudTexture)
+            // 一番後ろになるように設定
+            sprite.zPosition = -100
+            
+            // スプライトの表示する位置を指定する
+            sprite.position = CGPoint(
+                x: cloudTexture.size().width / 2 + cloudTexture.size().width * CGFloat(i),
+                y: self.size.height - cloudTexture.size().height / 2
+            )
+
+            // スプライトにアクションを設定する
+            sprite.run(repeatScrollCloud)
+
+            // シーンにスプライトを追加する
+            addChild(sprite)
+        }
+    }
+    
+    func setupWall() {
+        // 壁の画像を読み込む
+        let wallTexture = SKTexture(imageNamed: "wall")
+        wallTexture.filteringMode = .linear // 当たり判定を行うスプライトなので画質優先に。
+        
+        // 移動する距離を計算
+        let movingDistance = self.frame.size.width + wallTexture.size().width
+        
+        // 画面外まで移動するアクションを作成
+        let moveWall = SKAction.moveBy(x: -movingDistance, y: 0, duration: 4)
+        
+        // 自身を取り除くアクションを作成
+        let removeWall = SKAction.removeFromParent()
+        
+        // 2つのアニメーションを順に実行するアクションを作成
+        let wallAnimation = SKAction.sequence([moveWall,removeWall])
+        
+        // 鳥の画像サイズを取得
+        let birdSize = SKTexture(imageNamed: "bird_a").size()
+        
+        // 鳥が通り抜ける隙間の大きさを鳥のサイズの４倍とする
+        let slit_length = birdSize.height * 4
+        
+        // 隙間位置の上下の振れ幅を60ptとする
+        let random_y_range: CGFloat = 60
+
+        // 空の中央位置（y座標）をを取得
+        let groundSize = SKTexture(imageNamed: "ground").size()
+        let sky_center_y = groundSize.height + (self.frame.size.height - groundSize.height) / 2
+        
+        // 空の中央位置を基準にして下側の壁の中央位置を取得
+        let under_wall_center_y = sky_center_y - slit_length / 2 - wallTexture.size().height / 2
+        
+        // 壁を生成するアクションを作成
+        let createWallAnimation = SKAction.run({
+            // 壁をまとめるノードを作成
+            let wall = SKNode()
+            wall.position = CGPoint(x: self.frame.size.width + wallTexture.size().width / 2, y: 0)
+            // 雲より手前、地面より奥
+            wall.zPosition = -50
+            // 下側の壁の中央位置にランダム値を足して、下側の壁の表示位置を決定する
+            let random_y = CGFloat.random(in: -random_y_range...random_y_range)
+            let under_wall_y = under_wall_center_y + random_y
+
+            // 下側の壁を作成
+            let under = SKSpriteNode(texture: wallTexture)
+            under.position = CGPoint(x: 0, y: under_wall_y)
+            
+            // 壁をまとめるノードに下側の壁を追加
+            wall.addChild(under)
+            
+            // 上側の壁を作成
+            let upper = SKSpriteNode(texture: wallTexture)
+            upper.position = CGPoint(x: 0, y: under_wall_y + wallTexture.size().height + slit_length)
+            
+            // 壁をまとめるノードに上側の壁を追加
+            wall.addChild(upper)
+            
+            // 壁をまとめるノードにアニメーションを追加
+            wall.run(wallAnimation)
+            
+            // 壁を表示するノードに今回作成した壁を追加
+            self.wallNode.addChild(wall)
+        })
+        
+        // 次の壁作成までの時間待ちのアクションを作成
+        let waitAnimation = SKAction.wait(forDuration: 2)
+        
+        // 「壁を作成->時間まち->壁を作成」を無制限に繰り返すアクションを作成
+        let repeatForeverAnimation = SKAction.repeatForever(SKAction.sequence([createWallAnimation,waitAnimation]))
+        
+        // 壁を表示するノードに壁の作成を無限に繰り返すアクションを設定
+        wallNode.run(repeatForeverAnimation)
+        
+    }
+    
+    func setupBird() {
+        // 鳥の画像を２種類読み込む
+        let birdTextureA = SKTexture(imageNamed: "bird_a")
+        birdTextureA.filteringMode = .linear
+        let birdTextureB = SKTexture(imageNamed: "bird_b")
+        birdTextureB.filteringMode = .linear
+        
+        // ２種類のテクスチャを交互に変更するアニメーションを作成
+        let textureAnimation = SKAction.animate(with: [birdTextureA, birdTextureB], timePerFrame: 0.2)
+        let flap = SKAction.repeatForever(textureAnimation)
+        
+        // スプライトを作成
+        bird = SKSpriteNode(texture: birdTextureA)
+        bird.position = CGPoint(x: self.frame.size.width * 0.2, y: self.frame.size.height * 0.7)
+        
+        // アニメーション設定
+        bird.run(flap)
+        
+        // スプライトを追加する
+        addChild(bird)
+    }
+}
--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // SKView型に型変換する
+        let skView = self.view as! SKView
+        
+        // FPSを表示する
+        skView.showsFPS = true
+        
+        // ノードの数を表示
+        skView.showsNodeCount = true
+        
+        // ビューと同じサイズでシーンを作成する
+        let scene = GameScene(size:skView.frame.size)
+        
+        // ビューに表示する
+        skView.presentScene(scene)
     }
 
+    // ステータスバーを消す
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 
 }
 
